d3f278a94a27e00d9d91ffbabada0a72101b64b5
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2020-10-03 22:35:19 +0200

    fix: drop unnecessary test

--- husky.config.js
+++ husky.config.js
@@ -2,6 +2,6 @@ module.exports = {
   hooks: {
     'commit-msg': 'commitlint -e $HUSKY_GIT_PARAMS',
     'pre-commit': 'npm run update-badge && lint-staged',
-    'pre-push': 'npm test && npm run test-schema',
+    'pre-push': 'npm test',
   },
 };
