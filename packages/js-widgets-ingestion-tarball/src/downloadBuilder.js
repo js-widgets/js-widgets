ecc3e1c21fc4f1193bff35a5ef8e1218867711d6
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2020-11-30 17:11:02 +0100

    fix: avoid crash when using a local tarball

--- packages/js-widgets-ingestion-tarball/src/downloadBuilder.js
+++ packages/js-widgets-ingestion-tarball/src/downloadBuilder.js
@@ -7,7 +7,7 @@ const path = require('path');
 const remoteCopy = require('./remoteCopy');
 const urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)/;
 
-module.exports = async (getTarballUrl, options) =>
+module.exports = (getTarballUrl, options) =>
   async function (widget, destination) {
     const { headers, shortcode } = options;
     const url = await getTarballUrl(widget);
